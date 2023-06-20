d3f6b0f82f6a9f68b0af9f36dd7c3d7ceaa1d4b4
tapu18 <118423392+tapu18@users.noreply.github.com>
2023-06-20 19:10:30 +0900

    delete config.js

--- scripts/configsdfa.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    power:false, //ワールド起動時やreload時の初期電源状態(true/false)
-
-    check_no_player:false, //ワールド内にプレイヤーがいない時に動かすか(true/false)
-
-    polling_rate:160, //通常時のBDS→discordのチェック間隔(tick単位)
-}
-export default config
